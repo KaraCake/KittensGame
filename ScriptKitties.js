97f84a4d71dacec07198b821c6c726a159fc633e
KaraCake <vic9812@gmail.com>
2017-11-28 01:34:21 -0500

    Update ScriptKitties.js

--- ScriptKitties.js
+++ ScriptKitties.js
@@ -515,7 +515,7 @@ if (autoCheck[1] != "false") {
 for (var i = 0; i < resources.length; i++) {
     var curRes = gamePage.resPool.get(resources[i][0]);
     var resourcePerTick = gamePage.getResourcePerTick(resources[i][0], 0);
-    var resourcePerCraft = (resourcePerTick * 10);
+    var resourcePerCraft = (resourcePerTick * 50);
 		if (curRes.value > (curRes.maxValue - resourcePerCraft) && gamePage.workshop.getCraft(resources[i][1]).unlocked) {
 		gamePage.craft(resources[i][1], (resourcePerCraft / resources[i][2]));
 		}
